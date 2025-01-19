982b0180fd00d5f5f15a82b1dae0317954bdbabc
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2025-01-20 00:04:42 +0530

    fix deployment bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
-import About from "./Components/About/About";
+import About from "./Components/About/about";
 import Contact from "./Components/Contact/contact";
 function App() {
   return (
