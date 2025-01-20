6c9c9b8fd47a9b43a10c943bf045c89b4bf82e45
soumya-jain123 <soumya.11082005@gmail.com>
2025-01-20 08:51:50 +0530

    umpdated contact page

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -106,7 +106,7 @@ const Contact = () => {
   <iframe
     title="location"
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3440.710957264507!2d77.96426167619147!3d30.415941600872767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3908d4890d7c1735%3A0x22d3ae324c238e3c!2sUPES!5e0!3m2!1sen!2sin!4v1737310361608!5m2!1sen!2sin"
-    width="100%"
+    width="50%"
     height="400"
     style={{ border: 0 }}
     allowFullScreen=""
